7d9b4054d131dacd575c0d727eb0f9ebfe6f5cce
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2024-04-09 21:34:57 +0200

    Endpoint that receives measures. Fixes #2

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,14 @@
-use axum::{routing::get, Router};
+use axum::{routing::post, Json, Router};
+use chrono::{DateTime, Local};
+use serde::Deserialize;
 use tracing::info;
+
+#[derive(Deserialize)]
+struct SaveMeasureInput {
+    measure: String,
+    #[serde(serialize_with = "serialize_dt")]
+    recorded_at: DateTime<Local>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -8,7 +17,7 @@ async fn main() {
         .with_line_number(true)
         .init();
 
-    let app = Router::new().route("/", get(root));
+    let app = Router::new().route("/measure", post(save_measure));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -17,6 +26,7 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn save_measure(Json(input): Json<SaveMeasureInput>) -> &'static str {
+    println!("The value is {} the time: {}", input.measure, input.recorded_at);
+    "{}"
 }
