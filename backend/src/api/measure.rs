3720bba1d3bb973d57052589e441c6a77eb79984
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2024-04-13 00:19:37 +0200

    Integration tests for save_measure enpoint. #10

--- backend/src/api/measure.rs
+++ backend/src/api/measure.rs
@@ -1,11 +1,10 @@
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
+#[derive(Clone, Deserialize, Serialize)]
 pub struct SaveMeasureInput {
     pub device_id: String,
     pub measure: String,
-    #[serde(serialize_with = "serialize_dt")]
     pub recorded_at: DateTime<Local>,
 }
 
--- backend/tests/handler/measure.rs
+++ backend/tests/handler/measure.rs
@@ -3,9 +3,16 @@ use axum::{
     http,
     http::{Request, StatusCode},
 };
+use chrono::Local;
 use http_body_util::BodyExt;
 use serde_json::{json, Value};
-use smart_fluid_flow_meter_backend::storage::memory::MemoryStorage;
+use smart_fluid_flow_meter_backend::{
+    api::measure::{
+        SaveMeasureInput,
+        Measure,
+    },
+    storage::memory::MemoryStorage,
+};
 use std::sync::Arc;
 use tower::util::ServiceExt;
 
@@ -34,4 +41,67 @@ async fn save_measure_invalid_json() {
         body,
         json!({ "code": "InvalidInput", "message": "Invalid input" })
     );
+}
+
+#[tokio::test]
+async fn save_measure_success() {
+    let storage = Arc::new(MemoryStorage::new().await);
+    let app = smart_fluid_flow_meter_backend::app(storage).await;
+
+    let input = SaveMeasureInput {
+        device_id: "999".to_string(),
+        measure: "134".to_string(),
+        recorded_at: Local::now(),
+    };
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri("/measure")
+                .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                .body(Body::from(serde_json::to_string(&input).unwrap()))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let expected = Measure {
+        id: input.recorded_at.to_string(),
+        device_id: input.device_id.to_string(),
+        measure: input.measure.to_string(),
+        recorded_at: input.recorded_at,
+    };
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    assert_eq!(body, serde_json::to_value(expected).unwrap());
+}
+
+#[tokio::test]
+async fn save_measure_invalid_date() {
+    let storage = Arc::new(MemoryStorage::new().await);
+    let app = smart_fluid_flow_meter_backend::app(storage).await;
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri("/measure")
+                .header(http::header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+                .body(Body::from("{\"device_id\": \"1\", \"measure\": \"1\", \"recorded_at\":\"today\"}"))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::UNPROCESSABLE_ENTITY);
+
+    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body: Value = serde_json::from_slice(&body).unwrap();
+    assert_eq!(
+        body,
+        json!({ "code": "InvalidInput", "message": "Invalid input" })
+    );
+
 }
