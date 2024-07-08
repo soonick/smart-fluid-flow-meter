50909bb7bae4cb7dcda72f6cfa88836e204abb32
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2024-07-08 16:12:17 +0200

    Publish backend image to GCR

---
+++ scripts/publish-backend.sh
@@ -0,0 +1,24 @@
+# Exit if there is any error
+set -e
+# Echo all commands
+set -x
+
+# Constants
+REGISTRY_URL='us-central1-docker.pkg.dev'
+IMAGE_NAME="smart-fluid-flow-meter-backend-image-prod"
+
+# Log-in to remote docker as one of the first steps as we have no control
+# over the availability of the registry
+echo $JSON_KEY > keyfile.json
+cat keyfile.json | docker login -u _json_key_base64 --password-stdin https://$REGISTRY_URL
+
+# Get current tag number
+LAST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+
+# Build docker image
+cd backend
+make build-prod
+
+# Publish docker image
+docker tag $IMAGE_NAME $REGISTRY_URL/$PROJECT/$REPOSITORY/$IMAGE_NAME:$LAST_TAG
+docker push $REGISTRY_URL/$PROJECT/$REPOSITORY/$IMAGE_NAME:$LAST_TAG
