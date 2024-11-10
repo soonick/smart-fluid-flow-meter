0ad576a71c1e7b0adf4b7bfc586e46c32f9a54da
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2024-11-10 16:17:40 -0500

    Shutdown AP after getting config. Restart AP after factory reset

--- esp32/src/main/src/main.cpp
+++ esp32/src/main/src/main.cpp
@@ -20,7 +20,7 @@ const char* TAG = "smart-fluid-flow-meter";
 /**
  * WiFi configuration
  */
-wm_config config;
+wm_config wifi_config;
 
 /**
  * factory_reset task handle
@@ -33,15 +33,31 @@ TaskHandle_t factory_reset_handle;
 Button reset_button = Button(RESET_BUTTON);
 
 /**
+ * Wifi manager
+ */
+EspIdfWifiManager* wm = nullptr;
+
+/**
+ * Saves the wifi config and shuts down access point
+ */
+void save_config(wm_config in) {
+  wifi_config = in;
+  wm->shutdown_ap();
+  ESP_LOGI(TAG, "Wifi is configured");
+}
+
+/**
  * Monitors reset button and performs hard reset if conditions are met
  */
 void factory_reset(void* pvParameters) {
   (void)pvParameters;
 
   while (true) {
-    if (reset_button.is_long_pressed(5000)) {
-      ESP_LOGE(TAG, "Resetting meter");
-      // TODO: reset wifi manager
+    if (!wifi_config.ssid.empty() && reset_button.is_long_pressed(5000)) {
+      ESP_LOGI(TAG, "Resetting meter");
+      wm->clear_config();
+      wifi_config.ssid = "";
+      std::optional<wm_config> config_opt = wm->get_config(save_config);
     }
   }
 }
@@ -52,18 +68,18 @@ void factory_reset(void* pvParameters) {
 void power_led() {
   gpio_reset_pin(POWER_LED);
   gpio_set_direction(POWER_LED, GPIO_MODE_OUTPUT);
+  ESP_LOGI(TAG, "Turning on power LED");
   gpio_set_level(POWER_LED, 1);
 }
 
 extern "C" void app_main() {
-  EspIdfWifiManager* wm =
-      EspIdfWifiManager::get_instance("my-esp32-ssid", "APassword");
+  wm = EspIdfWifiManager::get_instance("my-esp32-ssid", "APassword");
 
-  std::optional<wm_config> config_opt =
-      wm->get_config([](wm_config in) { config = in; });
+  wifi_config.ssid = "";
+  std::optional<wm_config> config_opt = wm->get_config(save_config);
 
   if (config_opt.has_value()) {
-    config = config_opt.value();
+    save_config(config_opt.value());
   }
 
   xTaskCreate(factory_reset, "factory_reset", 4096, nullptr, tskIDLE_PRIORITY,
