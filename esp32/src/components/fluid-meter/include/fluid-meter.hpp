1a07ea9fe8beb3f13e6bc78283a35db512554597
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2024-11-12 19:11:32 -0500

    [esp32] Read measurements from flow meter (#62)

---
+++ esp32/src/components/fluid-meter/include/fluid-meter.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <driver/gpio.h>
+#include <freertos/FreeRTOS.h>
+
+/**
+ * This is a singleton so the Service Interrupt Routine can be a method
+ */
+class FluidMeter {
+ public:
+  /**
+   * Delete this constructos to make sure there can only be one instance
+   */
+  FluidMeter(FluidMeter const&) = delete;
+  void operator=(FluidMeter const&) = delete;
+
+  /**
+   * Reads measurements from given pin
+   *
+   * @param pin - Pin where measurements are going to be read from
+   */
+  static FluidMeter* get_instance(const gpio_num_t pin);
+
+  /**
+   * Returns the number of litters that have been measured since last run
+   */
+  float get_volume();
+
+ private:
+  const gpio_num_t meter_pin;
+  const float conversion_factor;
+
+  static FluidMeter* instance;
+
+  /**
+   * It should only be modified from the queue task
+   */
+  static int total_pulses;
+
+  /**
+   * Used for serializing the interrupts and keeping `total_pulses` safe
+   */
+  static QueueHandle_t queue;
+
+  /**
+   * Private constructor. Used internally to create the singleton
+   */
+  FluidMeter(const gpio_num_t pin);
+
+  /**
+   * Fluid meter interrupt callback. Counts the pulses emitted by the meter
+   */
+  static void count_pulses(void* args);
+
+  /**
+   * Consumes data from queue
+   */
+  static void queue_task(void* params);
+};
---
+++ esp32/src/components/fluid-meter/src/fluid-meter.cpp
@@ -0,0 +1,59 @@
+#include "fluid-meter.hpp"
+
+#include <driver/gpio.h>
+
+int FluidMeter::total_pulses = 0;
+FluidMeter* FluidMeter::instance = nullptr;
+QueueHandle_t FluidMeter::queue;
+
+FluidMeter::FluidMeter(const gpio_num_t pin)
+    : meter_pin{pin}, conversion_factor{450} {
+  queue = xQueueCreate(10, sizeof(char));
+  xTaskCreate(queue_task, "queue_task", 2048, NULL, 10, NULL);
+
+  gpio_config_t interrupt_config = {
+      .pin_bit_mask = 1ULL << meter_pin,
+      .mode = GPIO_MODE_INPUT,
+      .pull_up_en = GPIO_PULLUP_DISABLE,
+      .pull_down_en = GPIO_PULLDOWN_DISABLE,
+      .intr_type = GPIO_INTR_POSEDGE,
+  };
+  gpio_config(&interrupt_config);
+
+  gpio_install_isr_service(0);
+  gpio_isr_handler_add(meter_pin, this->count_pulses, nullptr);
+}
+
+FluidMeter* FluidMeter::get_instance(const gpio_num_t pin) {
+  if (FluidMeter::instance == nullptr) {
+    FluidMeter::instance = new FluidMeter(pin);
+  }
+
+  return FluidMeter::instance;
+}
+
+void FluidMeter::queue_task(void* params) {
+  char c;
+  while (true) {
+    if (xQueueReceive(queue, &c, portMAX_DELAY)) {
+      FluidMeter::total_pulses++;
+    }
+  }
+}
+
+void FluidMeter::count_pulses(void* args) {
+  char c = 1;
+  xQueueSendFromISR(queue, &c, nullptr);
+}
+
+float FluidMeter::get_volume() {
+  int pulses;
+
+  // Protect total_pulses by disabling interrupts
+  portDISABLE_INTERRUPTS();
+  pulses = this->total_pulses;
+  this->total_pulses = 0;
+  portENABLE_INTERRUPTS();
+
+  return pulses / conversion_factor;
+}
