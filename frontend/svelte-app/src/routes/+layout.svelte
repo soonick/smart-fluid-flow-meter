57c110f96ff446af2203635832f218010432c041
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2024-12-31 09:51:28 -0500

    [frontend] Add make vim command. Fixes #140

--- frontend/svelte-app/src/routes/+layout.svelte
+++ frontend/svelte-app/src/routes/+layout.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import '../app.css';
+  let { children } = $props();
 
-	import MdHeader from '../lib/components/MdHeader.svelte';
-	import MdNav from '../lib/components/MdNav.svelte';
+  import MdHeader from '../lib/components/MdHeader.svelte';
+  import MdNav from '../lib/components/MdNav.svelte';
 </script>
 
 <div>
-	<MdHeader />
-	<MdNav />
-	<section>
-		{@render children()}
-	</section>
-	<footer>All rights reserved</footer>
+  <MdHeader />
+  <MdNav />
+  <section>
+    {@render children()}
+  </section>
+  <footer>All rights reserved</footer>
 </div>
