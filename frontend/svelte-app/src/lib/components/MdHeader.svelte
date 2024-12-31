359da8a95515ea1603e0baa711f6449b2a45a74d
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2024-12-31 01:02:33 -0500

    [frontend] Simple login form. #107

---
+++ frontend/svelte-app/src/lib/components/MdHeader.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+</script>
+
+<style>
+header {
+  background: var(--primary-color);
+  padding: 1rem;
+  color: var(--secondary-color-weak);
+}
+</style>
+
+<header>
+  Mekadomus
+</header>
--- frontend/svelte-app/src/routes/+layout.svelte
+++ frontend/svelte-app/src/routes/+layout.svelte
@@ -1,6 +1,33 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import '../app.css';
+  let { children } = $props();
+
+  import MdHeader from '../lib/components/MdHeader.svelte';
+  import MdNav from '../lib/components/MdNav.svelte';
 </script>
 
-{@render children()}
+<style>
+:root {
+  --primary-color: #005f73;
+  --primary-color-weak: #4a5568;
+  --secondary-color: #ee6c4d;
+  --secondary-color-weak: #f7fafc;
+
+  --primary-text: #1a202c;
+}
+
+:global(body) {
+  margin: 0;
+  background: var(--secondary-color-weak);
+  color: var(--primary-text);
+}
+</style>
+
+<div>
+  <MdHeader/>
+  <MdNav />
+  <section>
+    {@render children()}
+  </section>
+  <footer>All rights reserved</footer>
+</div>
